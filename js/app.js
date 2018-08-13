59db6091b2c05144f70b5b38046bd20e55c78e4b
Jamie Strausbaugh <jstrausb86@gmail.com>
2018-08-13 19:09:43 -0400

    Fulfilled requirements for version one of Cat Clicker.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
+// To get the clicking events tracked on the page, you're going
+// to be working with an event listener which increments a number
+// on the page each time the cat image is clicked.
 
+let clickCount = 0;
+
+// Store the cat image into a variable
+const catImage = document.querySelector('img');
+
+catImage.addEventListener('click', function(){
+    //the element has been clicked... call function to increment clickCount
+    countClick();
+}, false);
+
+function countClick() {
+    // increment clickCount variable
+    clickCount++;
+    // get element holding click counter from document
+    const clickCounter = document.querySelector('.count');
+    // update the element's innerHTML with the latest value of clickCount
+    clickCounter.innerHTML = clickCount;
+}
